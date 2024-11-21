1050eb7301a2bd2296befdab75f067ed1f236f7b
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2024-11-21 20:24:16 +0100

    fix shift row view start and end time are truncated
    need a better aproach

--- TurnosL9/ShiftRowView.swift
+++ TurnosL9/ShiftRowView.swift
@@ -13,47 +13,48 @@ struct ShiftRowView: View {
     var body: some View {
         HStack {
             Text(shift.name)
-                .frame(height: 48)
                 .font(.largeTitle)
                 .bold()
                 .rowTitleStyle()
+                .frame(width: 72, height: 48)
             
             Spacer()
             
-            VStack(alignment: .leading) {
-                LabeledContent("Inicio") {
-                    Text(shift.shiftStart.positionalTimeString)
-                        .monospacedStyle()
-                }
-                LabeledContent("Fin") {
-                    Text(shift.shiftEnd.positionalTimeString)
-                        .monospacedStyle()
-                }
-            }
-            .font(.callout)
-            .frame(width: 100, alignment: .leading)
-            
-            Spacer()
-            
-            VStack(alignment: .leading) {
-                LabeledContent("Jornada") {
-                    Text(shift.shiftDuration.positionalTimeString)
-                        .monospacedStyle()
-                }
-                if let saturation = shift.saturation {
-                    LabeledContent("Saturación") {
-                        Text("\(saturation.formatted()) %")
-                            .fontWeight(.semibold)
+            HStack(spacing: 4) {
+                VStack(alignment: .leading) {
+                    LabeledContent("Inicio") {
+                        Text(shift.shiftStart.positionalTimeString)
+                            .monospacedStyle()
                     }
-                } else {
-                    Text("")
+                    LabeledContent("Fin") {
+                        Text(shift.shiftEnd.positionalTimeString)
+                            .monospacedStyle()
+                    }
                 }
+                .font(.callout)
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    LabeledContent("Jornada") {
+                        Text(shift.shiftDuration.positionalTimeString)
+                            .monospacedStyle()
+                    }
+                    if let saturation = shift.saturation {
+                        LabeledContent("Saturación") {
+                            Text("\(saturation.formatted()) %")
+                                .fontWeight(.semibold)
+                        }
+                    } else {
+                        Text("")
+                    }
+                }
+                .font(.callout)
             }
-            .font(.callout)
-            .frame(width: 150, alignment: .leading)
         }
         .rowStyle()
         .lineLimit(1)
+        .minimumScaleFactor(0.8)
     }
 }
 
