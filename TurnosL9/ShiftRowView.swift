90a270e702b92a47cd2d6ecd106fcd4af301fbb4
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2024-11-24 11:34:52 +0100

    add dynamic type size

--- TurnosL9/ShiftRowView.swift
+++ TurnosL9/ShiftRowView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ShiftRowView: View {
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
     let shift: Shift
-
+    
     var body: some View {
+        createRow(for: dynamicTypeSize)
+    }
+}
+
+#if DEBUG
+#Preview {
+    ShiftRowView(shift: .preview)
+        .padding(.horizontal)
+}
+#endif
+
+extension ShiftRowView {
+    var smallShiftRow: some View {
         HStack {
             Text(shift.name)
                 .font(.largeTitle)
@@ -32,6 +46,7 @@ struct ShiftRowView: View {
                     }
                 }
                 .font(.callout)
+                .frame(width: 100, alignment: .leading)
                 
                 Spacer()
                 
@@ -50,17 +65,188 @@ struct ShiftRowView: View {
                     }
                 }
                 .font(.callout)
+                .frame(width: 150, alignment: .leading)
             }
         }
         .rowStyle()
         .lineLimit(1)
-        .minimumScaleFactor(0.8)
+    }
+    
+    var mediumShiftRow: some View {
+        HStack {
+            Text(shift.name)
+                .font(.largeTitle)
+                .bold()
+                .rowTitleStyle()
+                .frame(width: 72, height: 48)
+            
+            HStack(spacing: 4) {
+                VStack(alignment: .leading) {
+                    LabeledContent("Inicio") {
+                        Text(shift.shiftStart.positionalTimeString)
+                            .monospacedStyle()
+                    }
+                    LabeledContent("Fin") {
+                        Text(shift.shiftEnd.positionalTimeString)
+                            .monospacedStyle()
+                    }
+                }
+                .font(.callout)
+                .frame(width: 120, alignment: .leading)
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    LabeledContent("Jorn.") {
+                        Text(shift.shiftDuration.positionalTimeString)
+                            .monospacedStyle()
+                    }
+                    if let saturation = shift.saturation {
+                        LabeledContent("Sat.") {
+                            Text("\(saturation.formatted()) %")
+                                .fontWeight(.semibold)
+                        }
+                    } else {
+                        Text("")
+                    }
+                }
+                .font(.callout)
+                .frame(width: 130, alignment: .leading)
+            }
+        }
+        .rowStyle()
+        .lineLimit(1)
+    }
+    
+    var largeShiftRow: some View {
+        HStack(alignment: .top) {
+            Text(shift.name)
+                .font(.largeTitle)
+                .bold()
+                .rowTitleStyle()
+                .frame(width: 80, height: 48)
+                .padding(.top, 16)
+            
+            VStack(alignment: .leading, spacing: 2) {
+                LabeledContent("Inicio") {
+                    Text(shift.shiftStart.positionalTimeString)
+                        .monospacedStyle()
+                }
+                LabeledContent("Fin") {
+                    Text(shift.shiftEnd.positionalTimeString)
+                        .monospacedStyle()
+                }
+                
+                LabeledContent("Jornada") {
+                    Text(shift.shiftDuration.positionalTimeString)
+                        .monospacedStyle()
+                }
+                if let saturation = shift.saturation {
+                    LabeledContent("Saturación") {
+                        Text("\(saturation.formatted()) %")
+                            .fontWeight(.semibold)
+                    }
+                } else {
+                    Text("")
+                }
+            }
+            .font(.callout)
+        }
+        .rowStyle()
+        .lineLimit(1)
+    }
+    
+    var xLargeShiftRow: some View {
+        HStack(alignment: .top) {
+            Text(shift.name)
+                .font(.largeTitle)
+                .bold()
+                .rowTitleStyle()
+                .frame(width: 90, height: 48)
+                .padding(.top, 18)
+            
+            VStack(alignment: .leading, spacing: 2) {
+                LabeledContent("Inicio") {
+                    Text(shift.shiftStart.positionalTimeString)
+                        .monospacedStyle()
+                }
+                LabeledContent("Fin") {
+                    Text(shift.shiftEnd.positionalTimeString)
+                        .monospacedStyle()
+                }
+                
+                LabeledContent("Jor.") {
+                    Text(shift.shiftDuration.positionalTimeString)
+                        .monospacedStyle()
+                }
+                if let saturation = shift.saturation {
+                    LabeledContent("Sat.") {
+                        Text("\(saturation.formatted()) %")
+                            .fontWeight(.semibold)
+                    }
+                } else {
+                    Text("")
+                }
+            }
+            .font(.callout)
+        }
+        .rowStyle()
+        .lineLimit(1)
+    }
+    
+    var xxLargeShiftRow: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            HStack {
+                Text(shift.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .rowTitleStyle()
+                    .frame(width: 96, height: 48)
+            }
+            .padding(.vertical,24)
+            .frame(maxWidth: .infinity, alignment: .center)
+            
+            LabeledContent("Inicio") {
+                Text(shift.shiftStart.positionalTimeString)
+                    .monospacedStyle()
+            }
+            LabeledContent("Fin") {
+                Text(shift.shiftEnd.positionalTimeString)
+                    .monospacedStyle()
+            }
+            
+            LabeledContent("Jorn.") {
+                Text(shift.shiftDuration.positionalTimeString)
+                    .monospacedStyle()
+            }
+            if let saturation = shift.saturation {
+                LabeledContent("Satu.") {
+                    Text("\(saturation.formatted()) %")
+                        .fontWeight(.semibold)
+                }
+            } else {
+                Text("")
+            }
+        }
+        .font(.callout)
+        .rowStyle()
+        .lineLimit(1)
     }
 }
 
-#if DEBUG
-#Preview {
-    ShiftRowView(shift: .preview)
-        .padding(.horizontal)
+extension ShiftRowView {
+    @ViewBuilder
+    private func createRow(for size: DynamicTypeSize) -> some View {
+        if size > .accessibility3 {
+            xxLargeShiftRow
+        } else if size > .accessibility1 {
+            xLargeShiftRow
+        } else if size > .xxLarge {
+            largeShiftRow
+        } else if size > .large {
+            mediumShiftRow
+        } else {
+            smallShiftRow
+        }
+    }
 }
-#endif
--- TurnosL9/TripRowView.swift
+++ TurnosL9/TripRowView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct TripRowView: View {
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
     let trip: Trip
     let color: Color
     let showIndicator: Bool
@@ -15,40 +16,19 @@ struct TripRowView: View {
     var opacity: Double {
         trip.isRunning && showIndicator ? 1 : 0
     }
-
+    
     var body: some View {
-        HStack {
-            Text(trip.train)
-                .rowTitleStyle()
-            
-            Spacer()
-            
-            Group {
-                VStack(alignment: .center) {
-                    Text(trip.origin)
-                    Text(trip.tripDeparture.positionalTimeString)
-                        .monospacedStyle()
-                }
-
-                VStack(alignment: .center) {
-                    Text(trip.destination)
-                    Text(trip.tripArrival.positionalTimeString)
-                        .monospacedStyle()
+        createRow(for: dynamicTypeSize)
+            .rowStyle(in: color)
+            .overlay(alignment: .topLeading) {
+                TimelineView(.animation) { _ in
+                    Circle()
+                        .frame(width: 8, height: 8)
+                        .foregroundStyle(.red)
+                        .offset(x: -12, y: trip.tripIndicatorPosition)
+                        .opacity(opacity)
                 }
             }
-            .font(.callout)
-            .frame(width: 125, alignment: .center)
-        }
-        .rowStyle(in: color)
-        .overlay(alignment: .topLeading) {
-            TimelineView(.animation) { _ in
-                Circle()
-                    .frame(width: 8, height: 8)
-                    .foregroundStyle(.red)
-                    .offset(x: -12, y: trip.tripIndicatorPosition)
-                    .opacity(opacity)
-            }
-        }
     }
 }
 
@@ -58,3 +38,155 @@ struct TripRowView: View {
         .padding(.horizontal)
 }
 #endif
+
+extension TripRowView {
+    var smallTripRow: some View {
+        HStack {
+            Text(trip.train)
+                .rowTitleStyle()
+                .frame(width: 72)
+            
+            Spacer()
+            
+            Group {
+                VStack(alignment: .center) {
+                    Text(trip.origin)
+                    Text(trip.tripDeparture.positionalTimeString)
+                        .monospacedStyle()
+                }
+                
+                VStack(alignment: .center) {
+                    Text(trip.destination)
+                    Text(trip.tripArrival.positionalTimeString)
+                        .monospacedStyle()
+                }
+            }
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+    
+    var mediumTripRow: some View {
+        HStack {
+            Text(trip.train)
+                .rowTitleStyle()
+                .frame(width: 90)
+            
+            Spacer()
+            
+            Group {
+                VStack(alignment: .center) {
+                    Text(trip.origin)
+                    Text(trip.tripDeparture.positionalTimeString)
+                        .monospacedStyle()
+                }
+                
+                VStack(alignment: .center) {
+                    Text(trip.destination)
+                    Text(trip.tripArrival.positionalTimeString)
+                        .monospacedStyle()
+                }
+            }
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+    
+    var largeTripRow: some View {
+        HStack {
+            Text(trip.train)
+                .rowTitleStyle()
+                .frame(width: 110)
+            
+            Spacer()
+            
+            Group {
+                VStack(alignment: .center) {
+                    Text(trip.origin)
+                    Text(trip.tripDeparture.positionalTimeString)
+                        .monospacedStyle()
+                }
+                
+                VStack(alignment: .center) {
+                    Text(trip.destination)
+                    Text(trip.tripArrival.positionalTimeString)
+                        .monospacedStyle()
+                }
+            }
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+    
+    var xLargeTripRow: some View {
+        HStack {
+            Text(trip.train)
+                .rowTitleStyle()
+                .frame(width: 120)
+            
+            Spacer()
+            
+            VStack(alignment: .leading, spacing: 0) {
+                LabeledContent {
+                    Text(trip.tripDeparture.positionalTimeString)
+                        .monospacedStyle()
+                } label: {
+                    Text(trip.origin)
+                }
+                
+                LabeledContent {
+                    Text(trip.tripArrival.positionalTimeString)
+                        .monospacedStyle()
+                } label: {
+                    Text(trip.destination)
+                }
+            }
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+    
+    var xxLargeTripRow: some View {
+        VStack(alignment: .leading) {
+            Text(trip.train)
+                .rowTitleStyle()
+                .frame(width: 160)
+                        
+            VStack(alignment: .leading, spacing: 0) {
+                LabeledContent {
+                    Text(trip.tripDeparture.positionalTimeString)
+                        .monospacedStyle()
+                } label: {
+                    Text(trip.origin)
+                }
+                
+                LabeledContent {
+                    Text(trip.tripArrival.positionalTimeString)
+                        .monospacedStyle()
+                } label: {
+                    Text(trip.destination)
+                }
+            }
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+}
+
+extension TripRowView {
+    @ViewBuilder
+    private func createRow(for size: DynamicTypeSize) -> some View {
+                if size > .accessibility2 {
+                    xxLargeTripRow
+                } else if size > .accessibility1 {
+                    xLargeTripRow
+                } else
+        if size > .xxxLarge {
+            largeTripRow
+                } else if size > .medium {
+            mediumTripRow
+        } else {
+            smallTripRow
+        }
+    }
+}
