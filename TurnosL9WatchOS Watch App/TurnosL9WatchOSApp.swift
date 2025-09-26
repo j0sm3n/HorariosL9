514a95f80ff574219830376dbc9ff18d5b24e0ad
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2025-09-26 07:32:20 +0200

    Add watchos target

---
+++ TurnosL9WatchOS Watch App/TurnosL9WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnosL9WatchOSApp.swift
+//  TurnosL9WatchOS Watch App
+//
+//  Created by Jose Antonio Mendoza on 26/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TurnosL9WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
